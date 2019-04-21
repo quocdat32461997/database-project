b5c0360e6b08f1a2eaef594e859ea1df8952c388
Dat Quoc Ngo <quocdat32461997@yahoo.com>
2019-04-21 13:08:18 -0500

    Update on generate_data & insert_data. Adding test

--- initialize_database.py
+++ initialize_database.py
@@ -3,6 +3,9 @@ import csv
 mysql = mysql.connector.connect(host="localhost", user = "root", password = "32461997")
 cursor = mysql.cursor(buffered=True)
 
+#drop database
+cursor.execute("DROP DATABASE IF EXISTS Spy")
+
 #open and read commands
 with open("SPY_DB_SETUP.sql", "r") as sql:
     sqlFile = sql.read()
@@ -13,12 +16,13 @@ for command in sqlCommands:
     try:
         cursor.execute(command)
     except Exception as e:
-        print('error')
-
+        print(e)
+cursor.execute("SHOW TABLES")
+for x in cursor:
+    print(x)
 #check if database is created
 cursor.execute("SHOW DATABASES")
 for x in cursor:
     print(x)
-
 #commit changes
 mysql.commit()
